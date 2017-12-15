2520cfb69b5a7d0e65e1b3630463e5731dff1709
StephenWu <wjwjq456@gmail.com>
2017-12-15 16:55:53 +0800

    vue webpack dev env

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import app from "views/layout/";
 import "styles/init.less";
 import "styles/fonts.less";
 import "styles/global.less";
-import "element-ui/lib/theme-chalk/index.css";
 
 Vue.component(Tabs.name, Tabs);
 Vue.component(TabPane.name, TabPane);
