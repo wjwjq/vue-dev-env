ed273211b7958994b1f485d9839351523e097880
StephenWu <wjwjq456@gmail.com>
2017-12-15 15:59:01 +0800

    vue webpack dev env

--- src/lib/utils/axios.js
+++ src/lib/utils/axios.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
-import store from 'store/';
+import axios from "axios";
+import store from "store/";
 import {
   AUTH_FAIL,
   SIGN_OUT
-} from 'store/user/constant';
+} from "store/user/constant";
 
-import qs from 'qs';
+import qs from "qs";
 
 import {
   request
-} from 'http';
+} from "http";
 import {
   error
-} from 'util';
+} from "util";
 
 const instance = axios.create();
 
 Promise.stop = () => new Promise(() => {});
 
 
-const dateFormat = (date) => {
+const dateFormat = date => {
   if (!date) {
     throw new Error(`argument 'date' is required`);
   }
@@ -27,22 +27,22 @@ const dateFormat = (date) => {
     date = new Date(date);
   }
   const o = {
-    'M+': date.getMonth() + 1, //月份
-    'd+': date.getDate(), //日
-    'h+': date.getHours(), //小时
-    'm+': date.getMinutes(), //分
-    's+': date.getSeconds(), //秒
-    'q+': Math.floor((date.getMonth() + 3) / 3), //季度
-    'S': date.getMilliseconds() //毫秒
+    "M+": date.getMonth() + 1, //月份
+    "d+": date.getDate(), //日
+    "h+": date.getHours(), //小时
+    "m+": date.getMinutes(), //分
+    "s+": date.getSeconds(), //秒
+    "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+    "S": date.getMilliseconds() //毫秒
   };
-  let fmt = 'yyyy-MM-dd hh:mm:ss';
-  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  let fmt = "yyyy-MM-dd hh:mm:ss";
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
   for (let k in o)
-    if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
   return fmt;
-}
+};
 
-const globalAxiosErrorHandler = (errResponse) => {
+const globalAxiosErrorHandler = errResponse => {
   const host = window.location.origin;
   const {
     timestamp,
@@ -52,7 +52,7 @@ const globalAxiosErrorHandler = (errResponse) => {
     method
   } = errResponse;
   console.error(`${dateFormat(timestamp)}  ${method.toUpperCase()}: ${host}${path}  ${status} (${error})`);
-}
+};
 
 async function networkCall(axiosPromise) {
   let resData = null;
@@ -82,14 +82,14 @@ async function networkCall(axiosPromise) {
 }
 
 const get = (url, params) => networkCall(instance(url, {
-  withCredentials: 'include',
+  withCredentials: "include",
   params
 }));
 
 
 const post = (url, postData) => networkCall(instance(url, {
-  method: 'POST',
-  withCredentials: 'include',
+  method: "POST",
+  withCredentials: "include",
   // headers: {
   //   'Content-type': 'application/json;charset=UTF-8'
   // },
@@ -98,8 +98,8 @@ const post = (url, postData) => networkCall(instance(url, {
 }));
 
 const put = (url, postData) => networkCall(instance(url, {
-  method: 'PUT',
-  withCredentials: 'include',
+  method: "PUT",
+  withCredentials: "include",
   // headers: {
   //     'Content-type': 'application/json;charset=UTF-8'
   // },
@@ -107,22 +107,22 @@ const put = (url, postData) => networkCall(instance(url, {
 }));
 
 const del = (url, postData) => networkCall(instance(url, {
-  method: 'DELETE',
-  withCredentials: 'include',
+  method: "DELETE",
+  withCredentials: "include",
   data: postData
 }));
 
-const dealAll = requestArrayList => {}
+const dealAll = requestArrayList => {};
 
 async function all(requestArrayList) {
-  console.info('requestArrayList', requestArrayList);
+  console.info("requestArrayList", requestArrayList);
   let result = [];
   let r;
   try {
     await requestArrayList.map(requestPromise => requestPromise.then(resData => result.push(resData)).catch(error => console.info(error)));
     console.info(result);
   } catch (error) {
-    console.info('axios all error', error);
+    console.info("axios all error", error);
   }
 }
 
